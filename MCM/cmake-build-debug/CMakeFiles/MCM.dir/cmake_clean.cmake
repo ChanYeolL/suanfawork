750dcac87c7c9081f38a4facc5dca6819948c859
ChanYeolL <403427693@qq.com>
2020-05-04 21:01:58 +0800

    第二次作业MCM

---
+++ MCM/cmake-build-debug/CMakeFiles/MCM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MCM.dir/MCM.c.obj"
+  "CMakeFiles/MCM.dir/main.c.obj"
+  "MCM.exe"
+  "MCM.exe.manifest"
+  "MCM.pdb"
+  "libMCM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MCM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
